df1729d1eda487cbd6855d6ef975273e97ad5151
Francois van der Hoven <francois.vanderhoven@unimelb.edu.au>
2022-06-03 07:55:08 +1000

    Show stocks and messages via ajax

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,24 @@
+class StocksController < ApplicationController
+  def search
+    unless params[:stock].present?
+      respond_to do |format|
+        flash.now[:alert] = "Enter a symbol to search"
+        format.js { render partial: 'users/stock_result' }
+      end
+      return
+    end
+
+    @stock = Stock.new_lookup(params[:stock])
+    if @stock.nil?
+      respond_to do |format|
+        flash.now[:alert] = "Enter a valid symbol to search"
+        format.js { render partial: 'users/stock_result' }
+      end
+      return
+    end
+
+    respond_to do |format|
+      format.js { render partial: 'users/stock_result' }
+    end
+  end
+end
