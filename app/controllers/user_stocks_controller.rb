dda61bd3bddf29e19b33ac6091e16c587671be3e
Francois van der Hoven <francois.vanderhoven@unimelb.edu.au>
2022-06-11 08:24:24 +1000

    Add profile view of myself and friends including their stocks list

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,5 +1,9 @@
 class UserStocksController < ApplicationController
   def create
+    if current_user.reached_max_stock?
+      flash[:alert] = "You have reached your maximum limit for stock tracking!"
+      redirect_to return_path and return
+    end
     stock = Stock.check_db(params[:ticker])
     if stock.nil?
       stock = Stock.new_lookup(params[:ticker])
@@ -7,17 +11,21 @@ class UserStocksController < ApplicationController
     end
     if stock.nil?
       flash[:alert] = "Stock not found"
-      redirect_to my_portfolio_path and return
+      redirect_to return_path and return
     end
     if current_user.stocks.include?(stock)
       flash[:alert] = "You are already tracking this stock"
-      redirect_to my_portfolio_path and return
+      redirect_to return_path and return
     end
-
     user = current_user
     user.stocks << stock
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
-    redirect_to my_portfolio_path
+    redirect_to return_path
+  end
+
+  def add_stock_of_a_friend
+    return_path(show_profile_path + "?id=#{params['id']}")
+    create
   end
 
   def destroy
@@ -26,5 +34,16 @@ class UserStocksController < ApplicationController
     user_stock.destroy
     flash[:notice] = "#{stock.ticker} was successfully removed from portfolio"
     redirect_to my_portfolio_path
+  end
+
+  private
+
+  def return_path(path = 'my_portfolio')
+    return @return_path if @return_path
+    if path == 'my_portfolio'
+      @return_path = my_portfolio_path
+    else
+      @return_path = path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,35 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stock = current_user.stocks
   end
+
+  def show_profile
+    @user = User.find_by(id: params['id'])
+    if @user.nil?
+      flash[:alert] = "An invalid reference has been detected"
+    end
+  end
+
+  def search
+    unless params[:part_name].present?
+      respond_to do |format|
+        flash.now[:alert] = "Enter some part of the first or last name you have in mind."
+        format.js { render partial: 'users/user_result' }
+      end
+      return
+    end
+    @users = User.find_by_partial(params[:part_name])
+
+    if @users.none?
+      respond_to do |format|
+        flash.now[:alert] = "Try to search using a different partial."
+        format.js { render partial: 'users/user_result' }
+      end
+      return
+    end
+
+    respond_to do |format|
+      format.js { render partial: 'users/user_result' }
+    end
+
+  end
 end
